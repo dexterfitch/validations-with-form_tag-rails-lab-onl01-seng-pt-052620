8119832701fcad5c7695489d270c39bc204152f9
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 06:38:53 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :email` is unique
-  validates :phone_number` is exactly 10 digits long
+  validates :email, uniqueness: true
+  validates :phone_number, length: { is: 10 }
 end
