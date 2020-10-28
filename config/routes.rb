6afa271e932d3e36c49b3bd4a3e3b33064db4fac
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 06:57:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
+  get "/posts/:id", to: "posts#show"
 
 end
