c82790bc9752ab39eb29a23eed9f5761e818101c
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 07:01:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post "/authors", to: "authors#create"
   get "/posts/:id", to: "posts#show", as: "post"
   patch "/posts/:id", to: "posts#update"
-  get "/posts/:id/edit", to: "posts#edit"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
 
 end
